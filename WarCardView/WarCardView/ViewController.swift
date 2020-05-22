7d7a31a03eb3d6cb774697df70ac9a0848a10d04
lukesky79 <61410214+lukesky79@users.noreply.github.com>
2020-05-22 11:03:31 +0200

    Add files via upload

---
+++ WarCardView/WarCardView/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  WarCardView
+//
+//  Created by Lee-Roy Nadjar on 19/02/2020.
+//  Copyright © 2020 Lee-Roy Nadjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14 )
+        
+        
+        let rightNumber = Int.random(in: 2...14)
+       
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+          leftScore += 1
+          
+            leftScoreLabel.text = String(leftScore)
+        
+        }
+        else if leftNumber < rightNumber {
+          rightScore += 1
+            
+            rightScoreLabel.text = String(rightScore)
+        }
+        else {
+            
+        }
+    }
+    
+    
+    
+    
+    
+}
+
